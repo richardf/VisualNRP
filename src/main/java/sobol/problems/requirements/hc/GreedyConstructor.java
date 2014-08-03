7f98b6418327882f2711df3f8a6201ba4b767ce4
Richard Fuchshuber <richardfuch@gmail.com>
2014-08-03 17:14:59 -0300

    Removed unused code

--- src/main/java/sobol/problems/requirements/hc/GreedyConstructor.java
+++ src/main/java/sobol/problems/requirements/hc/GreedyConstructor.java
@@ -94,14 +94,4 @@ public class GreedyConstructor implements Constructor {
         }
         return weights;
     }
-    
-    private List<Integer> generateListOfSize(int size) {
-        List<Integer> list = new ArrayList<Integer>(size);
-
-        for (int i = 0; i < size; i++) {
-            list.add(i);
-        }
-
-        return list;
-    }     
 }
